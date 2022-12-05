8f7928b606a03aeae3fdb69805eddbccd9ea6094
Kazakovtsev Nikita <nikita_kazakovtsev@bk.ru>
2022-12-05 23:45:53 +0300

    TEST11

--- sq.sql
+++ sq.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS albums(
-	albums_id serial PRIMARY KEY,
+	albums_id SERIAL PRIMARY KEY,
 	name TEXT NOT NULL,
 	time INTEGER NOT  NULL
 );
 CREATE TABLE IF NOT EXISTS music (
-	music_id serial PRIMARY KEY,
+	music_id SERIAL PRIMARY KEY,
 	name_music TEXT,
 	time TIME,
 	albums_id INTEGER REFERENCES albums(albums_id)
@@ -12,36 +12,36 @@ CREATE TABLE IF NOT EXISTS music (
 
 
 CREATE TABLE IF NOT EXISTS Collection (
-	 Collection_ID serial PRIMARY KEY,
+	 Collection_ID SERIAL PRIMARY KEY,
 	 name TEXT,
 	 year INTEGER
 );
 
 CREATE TABLE IF NOT EXISTS Collection_music (
-	id serial PRIMARY KEY,
+	id SERIAL PRIMARY KEY,
 	Collection_id INTEGER REFERENCES Collection(Collection_ID),
 	music_id INTEGER REFERENCES music(music_id)
 );
 
 CREATE TABLE IF NOT EXISTS executor (
-	executor_id serial PRIMARY KEY,
+	executor_id SERIAL PRIMARY KEY,
 	name TEXT
 );
 
 CREATE TABLE IF NOT EXISTS albums_executor (
-	id serial PRIMARY KEY,
+	id SERIAL PRIMARY KEY,
 	albums_id INTEGER REFERENCES albums(albums_id),
 	executor_id INTEGER REFERENCES executor(executor_id)
 );
 
 
 CREATE TABLE IF NOT EXISTS genre (
-	genre_id serial PRIMARY KEY,
+	genre_id SERIAL PRIMARY KEY,
 	name_genre TEXT
 );
 
 CREATE TABLE IF NOT EXISTS executor_genre (
-	id serial PRIMARY KEY,
+	id SERIAL PRIMARY KEY,
 	executor_id INTEGER REFERENCES executor(executor_id),
 	genre_id INTEGER REFERENCES genre(genre_id)
 );
